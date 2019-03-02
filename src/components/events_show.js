2956d471a99084de5e8c0f61be496ba55b81b795
YumaUeno <y.ueno.1.1.1.5@gmail.com>
2019-03-02 17:02:25 +0900

    implement delete feature

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -3,12 +3,18 @@ import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom';
 
-import { postEvent } from '../actions';
+import { getEvent, deleteEvent, putEvent } from '../actions';
 
-class EventsNew extends Component {
+class EventsShow extends Component {
   constructor(props) {
     super(props)
     this.onSubmit = this.onSubmit.bind(this)
+    this.onDeleteClick = this.onDeleteClick.bind(this)
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params
+    if(id) this.props.getEvent(id)
   }
 
   renderField(field) {
@@ -21,8 +27,14 @@ class EventsNew extends Component {
     )
   }
 
+  async onDeleteClick() {
+    const { id } = this.props.match.params
+    await this.props.deleteEvent(id)
+    this.props.history.push('/')
+  }
+
   async onSubmit(values) {
-    await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push('/')
   }
 
@@ -39,6 +51,7 @@ class EventsNew extends Component {
         <div>
           <input type="submit" value="submit" disabled={pristine || submitting || invalid} />
           <Link to="/">Cancel</Link>
+          <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
         </div>
       </form>
       
@@ -55,9 +68,13 @@ const validate = values => {
   return errors
 }
 
+const mapStateProps = (state, ownProps) => {
+  const event = state.events[ownProps.match.params.id]
+  return { initialValues: event, event }
+}
 
-const mapDispatchToPorps = ({ postEvent })
+const mapDispatchToPorps = ({ deleteEvent, getEvent, putEvent })
 
-export default connect(null, mapDispatchToPorps)(
-  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+export default connect(mapStateProps, mapDispatchToPorps)(
+  reduxForm({ validate, form: 'eventShowForm', enableReinitialize: true })(EventsShow)
 )
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,11 +1,19 @@
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions'
+import { READ_EVENTS, DELETE_EVENT, READ_EVENT, UPDATE_EVENT, CREATE_EVENT } from '../actions'
 
 
 export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS:
       return _.mapKeys(action.res.data, 'id')
+    case DELETE_EVENT:
+      delete events[action.id]
+      return {...events}
+    case READ_EVENT:
+    case CREATE_EVENT:
+    case UPDATE_EVENT:
+      const data = action.res.data
+      return { ...events, [data.id]: data }
     default:
       return events
   }
